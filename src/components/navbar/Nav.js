27dd2f9da06d637c258876f236b158cc87cd933b
Leonardo Ledesma <leodesigner@MacBook-Air-de-Leonardo.local>
2019-09-12 13:23:44 -0500

    Avance de pagina del juego

---
+++ src/components/navbar/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Nav.css';
+
+function Nav() {
+  return (
+    <div className="background_Nav">
+      <div className="container">
+        <div className="center">
+            <p className="space">Lanzamiento</p>
+            <p className="space">Media</p>
+            <p className="logo"></p>
+            <p className="space">Biografia</p>
+            <p className="space">Contacto</p>
+          </div>
+        </div>
+      </div>
+  );
+}
+
+export default Nav;
---
+++ src/components/video/Video.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Video.css';
+
+function Video() {
+  return (
+    <div className="video">
+      <div className="containervideo">
+        <div className="contentvideo">
+          <div>
+            <h1 className="title_size">Titulo cualquiera</h1>
+            <br></br>
+            <p className="trailer_size">Ver trailer completo</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Video;
